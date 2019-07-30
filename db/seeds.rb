b627cad511ac086d361265c003e285182fe578c5
Rashad <rashad.walcott15@gmail.com>
2019-07-30 10:44:57 -0400

    Completes Horoscope seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-
-Horoscope.create( )
+Horoscope.destroy_all
+#Horoscope creation
+Horoscope.create(name: "Aries" )
+Horoscope.create(name: "Taurus" )
+Horoscope.create(name: "Gemini" )
+Horoscope.create(name: "Cancer")
+Horoscope.create(name: "Leo")
+Horoscope.create(name: "Virgo")
+Horoscope.create(name: "Libra")
+Horoscope.create(name: "Scorpio")
+Horoscope.create(name: "Sagittarius")
+Horoscope.create(name: "Capricorn")
+Horoscope.create(name: "Aquarius")
+Horoscope.create(name: "Pisces")
