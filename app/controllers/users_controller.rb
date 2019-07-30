f0c9c093ee25443f7f982eb6707b87cf7336e591
Aliciea Dawn Suechin <aliciea@Alicieas-MacBook-Pro.local>
2019-07-30 16:55:17 -0400

    finish full crud on user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,57 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all 
+    end
+
+    def show 
+        @user = User.find(params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create 
+        @user = User.create(user_params)
+        redirect_to @user
+    end
+
+    #private 
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :age, :user_name, :horoscope_id)
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user = User.update(params[:id])
+        redirect_to @user
+    end
+
+    def destroy
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
