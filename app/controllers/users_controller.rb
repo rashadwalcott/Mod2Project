6c01d61676b0ee6b0f69906cf113f208cfd44403
Rashad <rashad.walcott15@gmail.com>
2019-07-31 08:06:32 -0400

    Makes corrections to User Model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,19 @@
 class UsersController < ApplicationController
 
     def index
-        @users = User.all 
+        @users = User.all
     end
 
-    def show 
+    def show
         @user = User.find(params[:id])
     end
 
     def new
         @user = User.new
-        @horoscopes = Horoscope.all 
+        @horoscopes = Horoscope.all
     end
 
-    def create 
+    def create
         @user = User.create(user_params)
         if @user.valid?
             redirect_to user_path(@user)
@@ -26,11 +26,12 @@ class UsersController < ApplicationController
 
     def edit
         @user = User.find(params[:id])
+        @horoscopes = Horoscope.all
     end
 
     def update
         @user = User.find(params[:id])
-        @user = User.update(params[:id])
+        @user.update(user_params)
         redirect_to @user
     end
 
@@ -41,17 +42,17 @@ class UsersController < ApplicationController
     end
 
 
-    private 
+    private
 
     def user_params
         params.require(:user).permit(:first_name, :last_name, :age, :user_name, :horoscope_id)
     end
 
-    
 
-    
 
-    
+
+
+
 
 
 
